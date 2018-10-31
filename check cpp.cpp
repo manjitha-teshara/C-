16efbd1598504ce067ea5b9f641d90af4bdbbdb6
manjitha-teshara <teranteshata01@gmail.com>
2018-10-31 08:06:31 +0530

    check cpp

---
+++ check cpp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	std::string name;
+	cin>>name;
+	
+	cout<<"my name is  "<<name<<"\n";
+	
+	puts("You can now C++");//don't need \n for line break
+	
+	printf("This come from printf function ");//same as ,don't need \ for line break
+	return 0;
+}
